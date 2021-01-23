d586bba531c6a953de6690c9ef38e534e7a9fa79
Cjen1 <cjj39@cam.ac.uk>
2021-01-23 21:25:01 +0000

    refactor send_request.py

--- src/send_request.py
+++ src/send_request.py
@@ -1,15 +1,22 @@
 import yagmail
-import argparse
 
 oauth2_file = 'oauth2_creds.json'
 
-args = argparse.ArgumentParser()
-args.add_argument('target_email', str, 'Email address you want to send to.')
-args.add_argument('user_name', str, 'Name of the user, e.g. Richard Head.')
-args.add_argument('login_credentials', list, 'Name of the user, e.g. Richard Head.')
-args.add_argument('sender', list, "User's email address.")
+def send_email(msg, src, dst):
+    yag = yagmail.SMTP(src, oauth2_file=oauth2_file)
+    yag.send(to=dst, message=msg)
 
-message = templates.request_template(args.sender, args.target_email, args.user_name, args.login_credentials)
+def send_request(sender, target_email, username, login_credentials):
+    message = templates.request_template(sender, target_email, user_name, login_credentials)
+    send_email(message, sender, target_email)
 
-yag = yagmail.SMTP(args.sender, oauth2_file=oauth2_file)
-yag.send(to=args.target_email, message=message)
+if __name__ == "__main__":
+    import argparse
+
+    args = argparse.ArgumentParser()
+    args.add_argument('sender', list, "User's email address.")
+    args.add_argument('target_email', str, 'Email address you want to send to.')
+    args.add_argument('user_name', str, 'Name of the user, e.g. Richard Head.')
+    args.add_argument('login_credentials', list, 'Name of the user, e.g. Richard Head.')
+
+    send_request(args.sender, args.target_email, args.user_name, args.login_credentials)
